03a204e8ecb787bc82f1f62f7993838e4cd77750
Pramod Laha <pramodlaha.pm.08@gmail.com>
2025-02-16 07:49:08 +0545

    feat: Add Home Link to Header Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,15 +13,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased bg-gray-900 text-white">
-          <header className="w-full bg-gray-900 border-b-2 border-red-500/30 shadow-2xl sticky top-0 z-50">
-            <div className="max-w-6xl mx-auto px-6 py-6 flex justify-center items-center">
-              <span className="text-3xl font-extrabold text-red-500 tracking-wider animate-pulse flex items-center gap-3">
-                <GiDeathSkull className="text-3xl" />
-                HDC PMBC
-              </span>
-            </div>
-          </header>
-          <main className="max-w-6xl mx-auto p-6">{children}</main>
+        <header className="w-full bg-gray-900 border-b-2 border-red-500/30 shadow-2xl sticky top-0 z-50">
+          <div className="max-w-6xl mx-auto px-6 py-6 flex justify-center items-center">
+            <span className="text-3xl font-extrabold text-red-500 tracking-wider animate-pulse flex items-center gap-3">
+              <GiDeathSkull className="text-3xl" />
+              <a href="/">HDC PMBC</a>
+            </span>
+          </div>
+        </header>
+        <main className="max-w-6xl mx-auto p-6">{children}</main>
       </body>
     </html>
   );
