1733715c06f4467b7ed863f18c2626dfb99fcb5e
Pramod <pramodlaha@himalayacollege.edu.np>
2026-02-12 07:17:02 +0545

    fix: Adjust TeamTable endIndex for correct display of leaderboard teams

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -154,8 +154,8 @@ export default function SplitLeaderboard({ teams }: { readonly teams: Team[] }) 
 
   return (
     <div className="flex flex-row justify-center gap-6 w-full">
-      <TeamTable teams={sortedTeams} startIndex={0} endIndex={5} />
-      <TeamTable teams={sortedTeams} startIndex={5} endIndex={10} />
+      <TeamTable teams={sortedTeams} startIndex={0} endIndex={4} />
+      <TeamTable teams={sortedTeams} startIndex={4} endIndex={8} />
       {/* <TeamTable teams={sortedTeams} startIndex={10} endIndex={15} /> */}
       {/* <TeamTable teams={sortedTeams} startIndex={7} endIndex={9} /> */}
       {/* <TeamTable teams={sortedTeams} startIndex={8} endIndex={13} /> */}
