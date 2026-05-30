df637729e1da3c78a2d8bc54bc58a63c3b4ed8c1
Pramod <pramodlaha@himalayacollege.edu.np>
2026-05-30 22:31:11 +0545

    feat: Update title in MatchTable to reflect PUBG Mobile Campus League

--- components/MatchTable.tsx
+++ components/MatchTable.tsx
@@ -211,7 +211,7 @@ export default function MatchTable({ matchId }: MatchTableProps) {
               />
             </div>*/}
             <h1 className="text-4xl md:text-6xl font-bold text-[#07559D] tracking-tight">
-              FF <span className="text-[#E76F00]">WarZone</span>
+               PUBG Mobile <span className="text-[#E76F00]">Campus League</span>
             </h1>
           </motion.div>
 
